bfd35402360de59848c2938205570cfd6d1c80b4
peter-lyr <llydrp.ldp@gmail.com>
2024-05-06 22:55:29 +0800

    <leader>tg toggle

--- lua/dp_toggle.lua
+++ lua/dp_toggle.lua
@@ -142,14 +142,14 @@ end
 
 function M._map()
   require 'which-key'.register {
-    ['<leader>t'] = { name = 'toggle', },
-    ['<leader>td'] = { function() M.diff() end, 'diff', mode = { 'n', 'v', }, silent = true, },
-    ['<leader>tw'] = { function() M.wrap() end, 'wrap', mode = { 'n', 'v', }, silent = true, },
-    ['<leader>tn'] = { function() M.nu() end, 'nu', mode = { 'n', 'v', }, silent = true, },
-    ['<leader>tr'] = { function() M.renu() end, 'renu', mode = { 'n', 'v', }, silent = true, },
-    ['<leader>ts'] = { function() M.signcolumn() end, 'signcolumn', mode = { 'n', 'v', }, silent = true, },
-    ['<leader>tc'] = { function() M.conceallevel() end, 'conceallevel', mode = { 'n', 'v', }, silent = true, },
-    ['<leader>tk'] = { function() M.iskeyword() end, 'iskeyword', mode = { 'n', 'v', }, silent = true, },
+    ['<leader>tg'] = { name = 'toggle', },
+    ['<leader>tgd'] = { function() M.diff() end, 'diff', mode = { 'n', 'v', }, silent = true, },
+    ['<leader>tgw'] = { function() M.wrap() end, 'wrap', mode = { 'n', 'v', }, silent = true, },
+    ['<leader>tgn'] = { function() M.nu() end, 'nu', mode = { 'n', 'v', }, silent = true, },
+    ['<leader>tgr'] = { function() M.renu() end, 'renu', mode = { 'n', 'v', }, silent = true, },
+    ['<leader>tgs'] = { function() M.signcolumn() end, 'signcolumn', mode = { 'n', 'v', }, silent = true, },
+    ['<leader>tgc'] = { function() M.conceallevel() end, 'conceallevel', mode = { 'n', 'v', }, silent = true, },
+    ['<leader>tgk'] = { function() M.iskeyword() end, 'iskeyword', mode = { 'n', 'v', }, silent = true, },
   }
 end
 
